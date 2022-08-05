46d43f73e99b8997bacbd4f5830db83759125dcc
TatyanaMilyutkina <milytkinata@gmail.com>
2022-08-05 11:04:38 +0400

    correction of comments

--- src/test/java/ru/netology/rest/SelenideTest.java
+++ src/test/java/ru/netology/rest/SelenideTest.java
@@ -1,5 +1,6 @@
 package ru.netology.rest;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,8 +26,9 @@ public class SelenideTest {
     }
 
     String data = generateDate(3);
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         open("http://localhost:9999");
     }
 
@@ -34,9 +36,6 @@ public class SelenideTest {
     @Test
     void test1() {
 
-        //Configuration.holdBrowserOpen = true;
-        //open("http://localhost:9999");
-        //SelenideElement form = $(".form");
         $x("//input[@placeholder='Город']").val("Самара");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").val(data);
@@ -44,9 +43,8 @@ public class SelenideTest {
         $x("//input[@name='phone']").val("+79375286987");
         $x("//span[@class='checkbox__box']").click();
         $x("//span[@class='button__text']").click();
-        $x("//*[contains(text(), 'Забронировать')]").shouldBe(visible, Duration.ofSeconds(15));
-        $(byText("Успешно"));
-        $(byText("Встреча успешно забронирована на " + data));
+        $x("//div[@class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + data), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 }
